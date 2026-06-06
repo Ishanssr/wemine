acaa1789a65a1e1320c3b23c278dcfda9fa27fc6
Ishan Jangir <15h4nj4n81r@gmail.com>
2026-06-07 03:40:09 +0530

    Remove setup-admin endpoint

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -30,12 +30,6 @@ export class AuthController {
   }
 
   @Public()
-  @Post('setup-admin')
-  async setupAdmin() {
-    return this.auth.setupAdmin();
-  }
-
-  @Public()
   @Post('login')
   @HttpCode(200)
   async login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: any) {
